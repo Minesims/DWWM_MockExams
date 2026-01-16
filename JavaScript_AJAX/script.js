4da51ee542b4d4dcc8d4dc81e4562a3fbb0f745e
Minesims <minesims@live.fr>
2026-01-16 16:47:38 +0100

    Fix: Modified code to fluidify it

--- JavaScript_AJAX/script.js
+++ JavaScript_AJAX/script.js
@@ -1,19 +1,29 @@
 let charLoad = document.getElementById('loadCharacters');
 let charList = document.getElementById('characters');
+let charData = '';
+
+fetch('https://rickandmortyapi.com/api/character')
+.then(response => response.json())
+.then(data => {
+    charData = data.results;
+    return charData;
+})
+.catch(error => {
+    return error;
+})
 
 charLoad.addEventListener('click', () => {
-    fetch('https://rickandmortyapi.com/api/character')
-    .then(response => response.json())
-    .then(data => {
-        charData = data.results;
-        charData.forEach(char => {      
-            charList.innerHTML += ('<div class="card">' + '<h3>' + char.name +'</h3>' + '<img src=' + char.image +'>');
+    charData.forEach(char => {      
+            charList.innerHTML += (`<div class="card ${char.id}"><h3>${char.name}</h3><img src=${char.image}>`);
         });
-    })
-    .catch(error => {
-        console.log(`Erreur ${error}`);
-     })
 });
 
 let focusChar = document.getElementById('card');
+
+focusChar.addEventListener('click', () => {
+    charData.forEach(char => {      
+            
+        });
+});
+
 
