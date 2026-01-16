49aa653b66d83b9997d4fd858a2ed3465c2be12e
Minesims <minesims@live.fr>
2026-01-16 15:44:19 +0100

    Feat: Linked API to html and css

--- JavaScript_AJAX/script.js
+++ JavaScript_AJAX/script.js
@@ -5,11 +5,15 @@ charLoad.addEventListener('click', () => {
     fetch('https://rickandmortyapi.com/api/character')
     .then(response => response.json())
     .then(data => {
-        data.forEach(char => {
-            charList.innerHTML += char;
+        charData = data.results;
+        charData.forEach(char => {      
+            charList.innerHTML += ('<div class="card">' + '<h3>' + char.name +'</h3>' + '<img src=' + char.image +'>');
         });
     })
     .catch(error => {
         console.log(`Erreur ${error}`);
-    })
-})
+     })
+});
+
+let focusChar = document.getElementById('card');
+
