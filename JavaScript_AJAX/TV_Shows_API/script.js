a0b084fb5ff85502dd9e962a60d45d4ff2ee0979
Minesims <minesims@live.fr>
2026-02-08 16:59:15 +0100

    Feat: Added commentary on JS code

--- JavaScript_AJAX/TV_Shows_API/script.js
+++ JavaScript_AJAX/TV_Shows_API/script.js
@@ -3,25 +3,27 @@ const resultsList = document.getElementById("results");
 const detailsDiv = document.getElementById("details");
 
 // TODO : écouter l’événement input
+
 let encodeQuery = "";
 
 searchInput.addEventListener('keyup', () => {
     let inputValue = searchInput.value;
-    encodeQuery = encodeURIComponent(inputValue);
-    apiQuery();
+    encodeQuery = encodeURIComponent(inputValue); // J'encode la valeur à chaque saisie pour éviter tout problème de lecture 
+    apiQuery(); // J'appelle ma fonction AJAX
 })
+
 // TODO : faire la requête AJAX vers TVMaze
 
-let data = []
+let data = [] // Je créé un array vide qui récupérera les données du fichier JSON
 
 async function apiQuery() {
     try {
         const response = await fetch(`https://api.tvmaze.com/search/shows?q=${encodeQuery}`);
-        if (!response.ok) {
+        if (!response.ok) { // Je vérifie que le fetch renvoie correctement ce qui est demandé
             throw new Error(`${response.status} ${response.statusText}`);
         } else {
             data = await response.json();
-            filterShows();
+            filterShows(); // J'appelle ma fonction de filtrage du fichier JSON
         }
     } catch(error) {
         console.log('Erreur : ', error);
@@ -31,20 +33,20 @@ async function apiQuery() {
 
 // TODO : afficher la liste des séries
 
-let showPoster = "";
+let showPoster = ""; // Je définie une variable qui contiendra l'image à afficher
 
 function filterShows() {
     resultsList.innerHTML = "";
     data.forEach(result => {
-        if (result.show.image) {
+        if (result.show.image) { // J'initie une boucle pour vérifier si une image réduite est disponible pour la série actuelle
             showPoster = result.show.image.medium;
-        } else {
+        } else { // Je définie une image générique (récupéré sur le même site que l'API) en cas de valeur null
             showPoster = "https://static.tvmaze.com/images/no-img/no-img-portrait-text.png";
         }
 
-        resultsList.innerHTML += (`<li class="shows"><img width="200" src="${showPoster}" alt="${result.show.name} poster"><p class="showTitle">${result.show.name}</p> `)
+        resultsList.innerHTML += (`<li class="shows"><img width="200" src="${showPoster}" alt="${result.show.name} poster"><p class="showTitle">${result.show.name}</p> `) // Je modifie le DOM pour créer une liste des séries correspondantes
     });
-    showsList();
+    showsList(); // J'appelle ma fonction de sélection de série
 }
     
 // TODO : gérer le clic sur une série
@@ -57,26 +59,26 @@ function showsList() {
     
     showsArray.forEach(listedShow => {
         listedShow.addEventListener('click', () => {
-            showsArray.forEach(show => show.classList.remove('active'));
-            listedShow.classList.add('active');
+            showsArray.forEach(show => show.classList.remove('active')); // Je reset la classe active à toutes les puces de la liste
+            listedShow.classList.add('active'); // J'active la classe active à seulement la puce de la liste qui enregistre le clic
 
             // TODO : afficher les détails
         
-                let activeShowTitle = listedShow.querySelector('.showTitle').textContent;
-                let activeShow = data.find(foundShow => foundShow.show.name === activeShowTitle);
+                let activeShowTitle = listedShow.querySelector('.showTitle').textContent; // Je recherche le titre de ma puce actuellement sélectionnée
+                let activeShow = data.find(foundShow => foundShow.show.name === activeShowTitle); // Je définie une variable qui récupère la data recherchée selon le titre de ma puce selectionnée
         
-                if (activeShow) {
-                    if (activeShow.show.image) {
+                if (activeShow) { 
+                    if (activeShow.show.image) { // J'initie une nouvelle boucle pour vérifier si une image entière est disponible pour la série actuelle
                         showPoster = activeShow.show.image.original;
-                    } else {
+                    } else { // Je redéfinie une image générique (récupéré sur le même site que l'API) en cas de valeur null
                         showPoster = "https://static.tvmaze.com/images/no-img/no-img-portrait-text.png";
                     }
     
-                    let genreStr = activeShow.show.genres.join(' · ');
+                    let genreStr = activeShow.show.genres.join(' · '); // Je transforme mon array en string
         
                     detailsDiv.innerHTML = (`<section id="details"><h2>${activeShow.show.name}</h2><img width="400" src="${showPoster}" alt="${activeShow.show.name} poster"><p><em>${genreStr}</em></p><p>${activeShow.show.summary}</p><p>Première diffusion : ${activeShow.show.premiered}</p></section>`);
 
-                    detailsDiv.scrollIntoView({behavior: "smooth", block: "end"});
+                    detailsDiv.scrollIntoView({behavior: "smooth", block: "end"}); // Je demande à ma page de défiler de manière lisse jusqu'à la section modifiée
                 }
         });
     })
